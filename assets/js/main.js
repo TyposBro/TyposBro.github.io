a6f184ea0e71e3a9d0b71bec63d02eaedea30266
Azizbek Umidjonov <azizbek@unist.ac.kr>
2021-12-26 22:13:19 +0900

    header, nav-menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,46 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.querySelector("#nav-menu");
+const navToggle = document.querySelector("#nav-toggle");
+const navClose = document.querySelector("#nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLikn = document.querySelectorAll(".nav__link");
 
+function linkAction() {
+  const navMenu = document.querySelector("#nav-menu");
+  navMenu.classList.remove("show-menu");
+}
 
+navLikn.forEach((n) => n.addEventListener("click", linkAction));
 /*==================== ACCORDION SKILLS ====================*/
-
 
 /*==================== QUALIFICATION TABS ====================*/
 
-
 /*==================== SERVICES MODAL ====================*/
-
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-
 /*==================== TESTIMONIAL ====================*/
-
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/

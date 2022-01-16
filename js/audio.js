828d9bee9dd9146a42208cf689141c66670bd7e1
Azizbek Umidjonov <azizbek@unist.ac.kr>
2022-01-17 03:56:28 +0900

    add alert

--- js/audio.js
+++ js/audio.js
@@ -1,7 +1,9 @@
 function enableAutoplay() {
   if (window.confirm("Do you want to get a full experience?")) {
     document.getElementById("myAudio").play();
+    return;
   }
+  alert("We cannot play background audio");
 }
 
 // enableAutoplay();
